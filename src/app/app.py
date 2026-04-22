6574c9d47f9d2b0372bea2ada3a89e9b7722036d
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2026-04-22 13:07:56 -0400

    Entry Point

---
+++ src/app/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+def boot_app():
+    st.title("Resident Scheduling")
+
+    
+
+    #we want to cache the schedule so it doesn't re-run for on change events
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   boot_app()
